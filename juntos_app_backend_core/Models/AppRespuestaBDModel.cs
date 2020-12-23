cd28f3fe1914d5709b3358c2d2837b146ebd1ad0
Leonardo Quispe Morales <qm.leonardo@gmail.com>
2020-12-23 19:28:24 -0400

    borrando antiguo backend

--- juntos_app_backend_core/Models/AppRespuestaBDModel.cs
+++
@@ -1,9 +0,0 @@
-namespace juntos_app_backend_core.Models
-{
-    public class AppRespuestaBD
-    {
-        public string status { get; set; }
-        public string response { get; set; }
-        public string numsec { get; set; }
-    }
-}
--- juntos_app_backend_core/Models/AppRespuestaCoreModel.cs
+++
@@ -1,9 +0,0 @@
-namespace juntos_app_backend_core.Models
-{
-    public class AppRespuestaCore
-    {
-        public string status { get; set; }
-        public dynamic response { get; set; }
-        public string token { get; set; }
-    }
-}
